17590afcfab8c27b09f97b31946f947b09def172
Amir <amirlebba@gmail.com>
2024-01-26 19:57:26 +0200

    adjusting the mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import "./App.css";
 import Arrow from "./assets/icon-arrow.svg";
 import MarkerPosition from "./MarkerPosition";
+import validator from "validator";
 
 function App() {
   const [address, setAddress] = useState(null);
@@ -11,6 +12,10 @@ function App() {
   const [loading, setLoading] = useState(false); 
   const [error, setError] = useState(null); 
   const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
+   const isValidIPAddress = (ip) => {
+     return validator.isIP(ip, 4) || validator.isIP(ip, 6);
+   };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +31,7 @@ function App() {
 
         const data = await response.json();
         setAddress(data);
-        console.log(data);
+        
       } catch (err) {
         // Handle errors during fetch or processing
         setError(err.message || "Something went wrong");
@@ -42,7 +47,12 @@ function App() {
 
   const handleSearch = async () => {
     try {
+
       setLoading(true);
+
+      if (!isValidIPAddress(userInput)) {
+        throw new Error("Invalid IP address format");
+      }
 
       const response = await fetch(
         `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${userInput}`
@@ -79,23 +89,38 @@ function App() {
             <input
               type="text"
               placeholder="Search for any IP address"
+              value={userInput}
               onChange={(e) => setUserInput(e.target.value)}
-              style={{ borderColor: error ? "red" : "initial" }}
+              style={{
+                borderColor: error
+                  ? "red"
+                  : isValidIPAddress(userInput)
+                  ? "initial"
+                  : "red",
+              }}
             />
             <button type="submit" onClick={handleSearch}>
               <img src={Arrow} className="arrow" alt="" />
             </button>
             <article>
-              <h2>IP Address</h2>
-              <h1>{address?.ip}</h1>
-              <h2>Location</h2>
-              <h1>
-                {address?.location.city}, {address?.location.country}
-              </h1>
-              <h2>Timezone</h2>
-              <h1>{address?.location.timezone} </h1>
-              <h2>ISP</h2>
-              <h1>{address?.isp}</h1>
+              <div>
+                <h2>IP Address</h2>
+                <h1>{address?.ip}</h1>
+              </div>
+              <div>
+                <h2>Location</h2>
+                <h1>
+                  {address?.location.city}, {address?.location.country}
+                </h1>
+              </div>
+              <div>
+                <h2>Timezone</h2>
+                <h1>{address?.location.timezone} </h1>
+              </div>
+              <div>
+                <h2>ISP</h2>
+                <h1>{address?.isp}</h1>
+              </div>
             </article>
           </form>
         </article>
